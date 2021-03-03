d355037c393b624043e7b30408fd69a5ae1a9a72
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-03-02 18:12:20 -0600

    Started the participation

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -6,7 +6,17 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Programming today is a race between software engineers striving to build bigger " +
+                "and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far," +
+                "the universe is winning.");
+            Console.WriteLine($"What word would you like to search for in the above text?");
+            string answer = Console.ReadLine();
+
+            Console.WriteLine($"What word would you like to change it to?");
+            string change = Console.ReadLine();
+
+            Console.WriteLine("Sorry, I could not find your word {INSERT THEIR WORD HERE}."); 
+
         }
     }
 }
